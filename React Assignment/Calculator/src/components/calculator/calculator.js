e1025a0439f819bbdb682017de09653c5dd57a12
2arian3 <aryan.boukani2@gmail.com>
2022-01-01 11:02:38 +0330

    Fixed some bugs.

--- React Assignment/Calculator/src/components/calculator/calculator.js
+++ React Assignment/Calculator/src/components/calculator/calculator.js
@@ -10,64 +10,115 @@ export default function Calculator() {
     op: '='
   });
   const [result, setResult] = useState('0');
+  const [entered, setEntered] = useState(false);
+  const [lastOpNum, setLastOpNum] = useState(null);
 
   function calculation() {
-    return  state.op === '+' ? (+state.currentNum) + (+state.lastNum) :
-            state.op === '-' ? (+state.lastNum) - (+state.currentNum) :
-            state.op === 'x' ? (+state.currentNum) * (+state.lastNum) :
-            state.op === '÷' && (+state.currentNum) !== 0 ? (+state.lastNum) / (+state.currentNum) :
-            state.op === '=' ? +state.currentNum : 'undefined';
+    let firstNum = state.currentNum;
+    let secondNum = state.lastNum;
+    if (lastOpNum) {
+      if (['+', 'x'].includes(state.op)) {
+        secondNum = lastOpNum;
+      } else if (['-', '÷'].includes(state.op)) {
+        secondNum = state.currentNum;
+        firstNum = lastOpNum;
+      }
+    }
+    return  state.op === '+' ? (+secondNum) + (+firstNum) :
+            state.op === '-' ? (+secondNum) - (+firstNum) :
+            state.op === 'x' ? (+secondNum) * (+firstNum) :
+            state.op === '÷' && (+firstNum) !== 0 ? (+secondNum) / (+firstNum) :
+            state.op === '' ? +state.currentNum : 'undefined';
   }
 
   function handlePressedKey(pressedKey) {
     if (pressedKey) {
       switch (pressedKey) {
         case '+':
-          setResult(String(calculation()));
-          setState({
-            currentNum: '0',
-            lastNum: String(calculation()),
-            op: '+'
-          });
+          setLastOpNum(null);
+          if (entered) {
+            setResult(String(calculation()));
+            setState({
+              currentNum: '0',
+              lastNum: String(calculation()),
+              op: '+'
+            });
+          } else {
+            setState({
+              currentNum: '0',
+              lastNum: state.currentNum,
+              op: '+'
+            });
+          }
+          setEntered(false);
           break;
         case '-':
-          setResult(String(calculation()));
-          setState({
-            currentNum: '0',
-            lastNum: String(calculation()),
-            op: '-'
-          });
+          setLastOpNum(null);
+          if (entered) {
+            setResult(String(calculation()));
+            setState({
+              currentNum: '0',
+              lastNum: String(calculation()),
+              op: '-'
+            });
+          } else {
+            setState({
+              currentNum: '0',
+              lastNum: state.currentNum,
+              op: '-'
+            });
+          }
+          setEntered(false);
           break;
         case 'x':
-          setResult(String(calculation()));
-          setState({
-            currentNum: '0',
-            lastNum: String(calculation()),
-            op: 'x'
-          });
+          setLastOpNum(null);
+          if (entered) {
+            setResult(String(calculation()));
+            setState({
+              currentNum: '0',
+              lastNum: String(calculation()),
+              op: 'x'
+            });
+          } else {
+            setState({
+              currentNum: '0',
+              lastNum: state.currentNum,
+              op: 'x'
+            });
+          }
+          setEntered(false);
           break;
         case '÷':
+          setLastOpNum(null);
           let resultOfCalc = String(calculation());
-          if (resultOfCalc !== 'undefined') {
+          if (resultOfCalc !== 'undefined' && entered) {
             setResult(resultOfCalc);
             setState({
               currentNum: '0',
               lastNum: String(resultOfCalc),
               op: '÷'
             });
-          } else {
+          } else if (resultOfCalc === 'undefined') {
             setResult('Invalid Input');
             setTimeout(() => {
               setResult('0');
               setState({
                 currentNum: '0',
                 lastNum: '0',
-                op: '='
+                op: ''
               });
             }, 500);
+          } else {
+            setState({
+              currentNum: '0',
+              lastNum: state.currentNum,
+              op: '÷'
+            });
           }
+          setEntered(false);
           break;
         case '%':
+          setLastOpNum(null);
           setResult(String(parseFloat(state.currentNum) / 100));
           setState({
             currentNum: String(parseFloat(state.currentNum) / 100),
@@ -77,6 +128,7 @@ export default function Calculator() {
           break;
         case '.':
           if (!state.currentNum.includes('.')) {
+            setLastOpNum(null);
             setResult(state.currentNum + '.');
             setState({
               currentNum: state.currentNum + '.',
@@ -86,6 +138,7 @@ export default function Calculator() {
           }
           break;
         case '+/-':
+          setLastOpNum(null);
           setResult(String(-state.currentNum));
           setState({
             currentNum: String(-state.currentNum),
@@ -94,44 +147,53 @@ export default function Calculator() {
           });
           break;
         case '=':
-          if (state.op !== '=') {
+          if (state.op) {
+            setEntered(false);
             let resultOfCalc = String(calculation());
             if (resultOfCalc !== 'undefined') {
-              setResult(String(calculation()));
+              if (!lastOpNum) {
+                setLastOpNum(state.currentNum);
+              }
+              setResult(resultOfCalc);
               setState({
-                currentNum: String(calculation()),
-                lastNum: '0',
-                op: '='
+                currentNum: resultOfCalc,
+                lastNum: state.currentNum,
+                op: state.op
               });
             } else {
+              setLastOpNum(null);
               setResult('Invalid Input');
               setTimeout(() => {
                 setResult('0');
                 setState({
                   currentNum: '0',
                   lastNum: '0',
-                  op: '='
+                  op: ''
                 });
               }, 500);
             }
           }
           break;
         case 'C':
+          setLastOpNum(null);
+          setEntered(false);
           setResult('0');
           setState({
             currentNum: '0',
             lastNum: '0',
-            op: '='
+            op: ''
           });
           break;
         default:
-          const newValue = state.currentNum === '0' ? pressedKey : state.currentNum + pressedKey;
+          const newValue = state.currentNum === '0' || lastOpNum ? pressedKey : state.currentNum + pressedKey;
+          setEntered(true);
           setResult(newValue);
           setState({
             currentNum: newValue,
             lastNum: state.lastNum,
-            op: state.op
+            op: lastOpNum ? '' : state.op
           });
+          setLastOpNum(null);
           break;
       }
     }
